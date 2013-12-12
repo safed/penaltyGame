e44e93053037dd707d80bbbfa657fb4f6442e5f2
safed <fede.sgl@gmail.com>
2013-12-12 02:43:07 +0100

    2

--- src/com/project/android/penaltygame/Activity2.java
+++ src/com/project/android/penaltygame/Activity2.java
@@ -1,17 +1,20 @@
 package com.project.android.penaltygame;
 
+import java.util.Random;
+
 import android.content.pm.ActivityInfo;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class Activity2 extends FragmentActivity {
+public class Activity2 extends FragmentActivity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,43 +22,69 @@ public class Activity2 extends FragmentActivity {
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-		WindowManager.LayoutParams.FLAG_FULLSCREEN);
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
 		setContentView(R.layout.activity_2);
 
 
 		Button btnLeft = (Button) findViewById(R.id.btnLeft);
 		Button btnRight = (Button) findViewById(R.id.btnRight);
+		
+		btnLeft.setOnClickListener(this);
+		btnRight.setOnClickListener(this);
+		
 		ImageView imgPlayer = (ImageView) findViewById(R.id.imgViewPlayer);
 		imgPlayer.setBackgroundResource(R.drawable.p1);
-				
-		btnLeft.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				ImageView imgPlayer = (ImageView) findViewById(R.id.imgViewPlayer);
-				imgPlayer.setBackgroundResource(R.drawable.anim_player);
-				AnimationDrawable playerAnim = (AnimationDrawable) imgPlayer.getBackground();
-				
-				playerAnim.start();
-				imgPlayer.setBackgroundResource(R.drawable.p1);
-				
-			}
-		});
+					
+	}
+	
+	/*
+	 * Start playing
+	 */
+	public void run(int x) {
+		ImageView imgPlayer = (ImageView) findViewById(R.id.imgViewPlayer);
+		imgPlayer.setBackgroundResource(R.drawable.anim_player);
+		AnimationDrawable playerAnim = (AnimationDrawable) imgPlayer.getBackground();
 		
-		btnRight.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				ImageView imgPlayer = (ImageView) findViewById(R.id.imgViewPlayer);
-				imgPlayer.setBackgroundResource(R.drawable.anim_player);
-				AnimationDrawable playerAnim = (AnimationDrawable) imgPlayer.getBackground();
-				
-				playerAnim.start();
-				
-			}
-		});		
+		playerAnim.start();		
+	}
+	
+	
+	/*
+	 * Put the player in original position
+	 * @param ImageView where set the first frame
+	 */
+	
+	public void reset(ImageView iv) {
+		iv.setBackgroundResource(R.drawable.p1);
+		
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see android.view.View.OnClickListener#onClick(android.view.View)
+	 */
+	
+	public void onClick(View v) {
+		String where = ((Button) v).getText().toString();
+		
+		if(where == "btnRight") 
+			run(0);
+		if (where == "btnRight") 
+			run(1);
+		else
+			run(2);
+	}
+	
+	
+	/*
+	 * Generating a random int.
+	 * Return a value between 0 and 2
+	 */
+	
+	public int random() {
+		Random r = new Random();
+		return (r.nextInt(40)%3);
 	}
 
 
